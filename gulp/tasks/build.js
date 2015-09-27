616cf1cc0bdc405b7eda20eb5383e3c213650f7c
Fernando Moreira <nandomoreira.me@gmail.com>
2015-09-27 02:20:19 -0300

    fix gulp build task

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -5,6 +5,6 @@
 var gulp = require('gulp');
 var runSequence = require('run-sequence');
 
-gulp.task('default', ['watch'], function (cb) {
-  runSequence('watch', ['sass', 'js', 'images', 'fonts'], cb);
+gulp.task('build', function (cb) {
+  runSequence('html', ['sass', 'js', 'images', 'fonts'], cb);
 });
